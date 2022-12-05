8ff69b52bda3f0152838ba21a5ddb829739910df
Alan Chang <alan@alanchang.xyz>
2022-12-04 16:08:44 -0800

    Revise database connection to use pool and promisify

--- server/db.js
+++ server/db.js
@@ -1,20 +1,30 @@
 const mysql = require('mysql');
+const util = require('util');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.AWS_HOST,
   user: process.env.AWS_USER,
   password: process.env.AWS_PASSWORD,
 });
 
-connection.connect((e) => {
+pool.getConnection((e, connection) => {
   if (e) {
+    const codes = {
+      PROTOCOL_CONNECTION_LOST: 'Database connection was closed.',
+      ER_CON_COUNT_ERROR: 'Database has too many connections.',
+      ECONNREFUSED: 'Database connection was refused.',
+    };
     // eslint-disable-line
-    console.log('Error connecting to mysql');
-  } else {
+    console.error(codes[e.code]);
+  }
+  if (connection) {
+    pool.query(`USE ${process.env.AWS_DB_NAME};`);
     // eslint-disable-line
     console.log('Connected to mysql successfully');
   }
 });
 
-module.exports = { connection };
+pool.query = util.promisify(pool.query);
+
+module.exports = { pool };
