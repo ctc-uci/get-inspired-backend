52d7d6a175aaa61172d0c5bf6aa33d1fa3dd7ff2
KainoaNishida <kanishid@uci.edu>
2022-12-11 17:32:08 -0800

    Made the schema folder and placed the clams.sql file inside

---
+++ server/clams.sql
@@ -0,0 +1,11 @@
+CREATE TABLE clams{
+    FOREIGN KEY(clamId),
+    rakerId INTEGER,
+    lat DECIMAL,
+    long DECIMAL,
+    length INTEGER,
+    width INTEGER,
+    weight INTEGER,
+    comments TEXT,
+    image TEXT,
+};
