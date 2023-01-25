79c82c04413bb86f937fc56a84f827cd944448a1
Jacqueline Thai <jthai1201@gmail.com>
2023-01-24 17:21:05 -0800

    Worked with Avent to finish users routes.

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(
   }),
 );
 app.use(cookieParser());
+app.use(express.json());
 
 // Routes
 app.use('/user', userRouter);
--- routes/user.js
+++ routes/user.js
@@ -1,27 +1,45 @@
 // imports
 const express = require('express');
+const toUnnamed = require('named-placeholders')();
 const admin = require('../firebase');
-const pool = require('../server/db');
-const { isAlphaNumeric } = require('../common/utils');
+const { pool } = require('../server/db');
+const { isAlphaNumeric, keysToCamel } = require('../common/utils');
 
 // init userRouter
 const userRouter = express();
 
 // userRouter: create a user
-userRouter.post('/create', async (req, res) => {
+userRouter.post('/', async (req, res) => {
   try {
-    const { userId, email, role, firstName, lastName } = req.body;
-    isAlphaNumeric(userId, '@userRouter Attemped to create user with nonalphanumeric id');
-
-    // (NOTE andrew): the ctc-uci/auth-frontend code handles
-    // adding the user to the Firebase DB, so we only add to the NPO DB here
-    const newUser = await pool.query(
-      'INSERT INTO users (user_id, email, user_role, first_name, last_name) VALUES ($(userId), $(email), $(role), $(firstName), $(lastName)) RETURNING *',
-      { userId, email, role, firstName, lastName },
+    const { email, firstName, lastName, role, id } = req.body;
+    const [query, params] = toUnnamed(
+      `
+      INSERT INTO user (
+        email,
+        first_name,
+        last_name,
+        role,
+        id
+        )
+      VALUES (
+        :email,
+        :firstName,
+        :lastName,
+        :role,
+        :id
+      );
+      SELECT * FROM user WHERE id = :id`,
+      {
+        email,
+        firstName,
+        lastName,
+        role,
+        id,
+      },
     );
-    res.send(200).send({
-      newUser: newUser.rows[0],
-    });
+
+    const user = await pool.query(query, params);
+    res.status(200).json(keysToCamel(user));
   } catch (err) {
     res.status(400).send(err.mesage);
   }
@@ -30,62 +48,76 @@ userRouter.post('/create', async (req, res) => {
 // userRouter: read DB for all users
 userRouter.get('/', async (req, res) => {
   try {
-    const users = await pool.query('SELECT * FROM users');
-    res.send({
-      users: users.rows,
-    });
+    console.log('GET');
+    const users = await pool.query('SELECT * FROM user;');
+    res.status(200).json(keysToCamel(users));
   } catch (err) {
     res.status(400).send(err.mesage);
   }
 });
 
 // userRouter: read DB for a specific user by their ID
-userRouter.get('/:userId', async (req, res) => {
+userRouter.get('/:id', async (req, res) => {
   try {
     // get the user id and double check that it's alphanumeric
-    const { userId } = req.params;
-    isAlphaNumeric(userId, '@userRouter userID is not alphanumeric');
+    const { id } = req.params;
+    isAlphaNumeric(id, '@userRouter id is not alphanumeric');
 
-    // if so, query and send
-    const user = await pool.query('SELECT * FROM users WHERE user_id = $(userId)', { userId });
-    res.send({ user: user.rows[0] });
+    const [query, params] = toUnnamed('SELECT * FROM user WHERE id = :id', {
+      id,
+    });
+    const user = await pool.query(query, params);
+    res.status(200).json(keysToCamel(user));
   } catch (err) {
     res.status(400).send(err.message);
   }
 });
 
 // userRouter: update a user
-userRouter.put('/update/:userId', async (req, res) => {
+userRouter.put('/:id', async (req, res) => {
   try {
-    // get and sanity check params
-    const { userId } = req.params;
-    isAlphaNumeric(userId, '@userRouter userID is not alphanumeric');
-    const { email, firstName, lastName } = req.body;
-
-    // if so, update role
-    const user = await pool.query(
-      'UPDATE users SET email=$(email), first_name=$(firstName), last_name=$(lastName) WHERE user_id = $(userId)',
-      { userId, email, firstName, lastName },
+    const { email, firstName, lastName, role, id } = req.body;
+    const [query, params] = toUnnamed(
+      `UPDATE user
+         SET
+         ${email ? 'email = :email, ' : ''}
+         ${firstName ? 'first_name = :firstName, ' : ''}
+         ${lastName ? 'last_name = :lastName, ' : ''}
+         ${role ? 'role = :role, ' : ''}
+         id = :id
+        WHERE id = :id;
+      SELECT * FROM user WHERE id = :id`,
+      {
+        email,
+        firstName,
+        lastName,
+        role,
+        id,
+      },
     );
-    res.send({ user: user.rows[0] });
+    const updatedUser = await pool.query(query, params);
+    res.status(200).json(keysToCamel(updatedUser));
   } catch (err) {
-    res.status(400).send(err.message);
+    res.status(500).send(err.message);
   }
 });
 
 // userRouter: delete a user both in FIrebase and NPO DB
-userRouter.delete('/update/role/:userId', async (req, res) => {
+userRouter.delete('/:id', async (req, res) => {
   try {
     // get the user id and double check that it's alphanumeric
-    const { userId } = req.params;
-    isAlphaNumeric(userId, '@userRouter userID is not alphanumeric');
+    const { id } = req.params;
+    isAlphaNumeric(id, '@userRouter userID is not alphanumeric');
 
     // delete from Firebase, then NPO database
-    await admin.auth().deleteUser(userId);
-    await pool.query('DELETE FROM users WHERE user_id=$(userId)', { userId });
+    await admin.auth().deleteUser(id);
+    const [query, params] = toUnnamed(`DELETE from user WHERE id = :id;`, {
+      id,
+    });
+    await pool.query(query, params);
 
     // send response
-    res.status(200).send(`Deleted user with ID: ${userId}`);
+    res.status(200).send(`Deleted user with ID: ${id}`);
   } catch (err) {
     res.status(400).send(err.message);
   }
