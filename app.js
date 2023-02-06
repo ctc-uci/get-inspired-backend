278e11114ec91659d444572ba35428b2fa01c1c1
Kyle Doliente <66671859+krd311@users.noreply.github.com>
2023-02-05 16:16:49 -0800

    Create Survey Table & Corresponding Routes (#13)
    * made schema and started routes
    
    * create schema and start routes
    
    * finish routes
    
    * change 'db' to 'pool'
    
    * Fixed the routes
    
    ---------
    
    Co-authored-by: Jacqueline Thai <jthai1201@gmail.com>

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const surveysRouter = require('./routes/surveys.router');
 const rakersRouter = require('./routes/rakers.router');
-require('dotenv').config();
-
 const clamsRouter = require('./routes/clams.router');
 
+require('dotenv').config();
+
 const app = express();
-
-const PORT = process.env.PORT || 3001;
-
+app.use(express.json());
 app.use(
   cors({
     origin: `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`,
   }),
 );
-app.use(express.json());
 
+const PORT = process.env.PORT || 3001;
+
+app.use('/surveys', surveysRouter);
 app.use('/clams', clamsRouter);
-
-app.use(express.json());
 app.use('/rakers', rakersRouter);
 
 app.listen(PORT, () => {
