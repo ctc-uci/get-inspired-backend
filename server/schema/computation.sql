3229e890ca609f09353cc29e203f10f75fcf1c8c
Maithy Le <maithyle265@yahoo.com>
2023-04-27 23:21:51 -0700

    Fixed raker delete trigger

--- server/schema/computation.sql
+++ server/schema/computation.sql
@@ -127,7 +127,7 @@ BEGIN
     UPDATE computation
     SET
       `# people` = `# people` - 1,
-      `# man hours` = IFNULL((SELECT HOUR(TIMEDIFF(`End Time`, `Start Time`)) + MINUTE(TIMEDIFF(`End Time`, `Start Time`)) / 60 FROM raker WHERE raker.survey_id = OLD.survey_id), 0),
+      `# man hours` = IFNULL((SELECT SUM(HOUR(TIMEDIFF(`End Time`, `Start Time`)) + MINUTE(TIMEDIFF(`End Time`, `Start Time`)) / 60) FROM raker WHERE raker.survey_id = OLD.survey_id), 0),
       `clams/man hr` = IFNULL((SELECT COUNT(*) from clam WHERE clam.survey_id = OLD.survey_id) / (SELECT SUM(HOUR(TIMEDIFF(`End Time`, `Start Time`)) + MINUTE(TIMEDIFF(`End Time`, `Start Time`)) / 60) FROM raker WHERE raker.survey_id = OLD.survey_id), 0),
       `clam density` = IFNULL(((SELECT COUNT(*) from clam WHERE clam.survey_id = OLD.survey_id) / (SELECT SUM(`Rake Width` * `Rake Distance`) FROM raker WHERE raker.survey_id = OLD.survey_id)), 0),
       `area raked` = IFNULL((SELECT SUM(`Rake Width` * `Rake Distance`) FROM raker WHERE raker.survey_id = OLD.survey_id), 0)
