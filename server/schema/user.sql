b81f4440534cf30f57eb9a2f61277d179243cb04
leeaj8-uci <leeaj8@uci.edu>
2023-07-29 18:17:01 -0700

    Some basic formatting for the User schema

--- server/schema/user.sql
+++ server/schema/user.sql
@@ -1,7 +1,7 @@
 CREATE TABLE user(
-  id     VARCHAR(255) PRIMARY KEY,
+  id          VARCHAR(255) PRIMARY KEY,
   email       VARCHAR(255),
-  role   ENUM('admin', 'intern'),
+  role        ENUM('admin', 'intern'),
   first_name  VARCHAR(50),
   last_name   VARCHAR(50)
 );
